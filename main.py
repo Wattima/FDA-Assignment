b6d27567f3168776d3a60aea516d9a8ced40935f
Wattima <darwattima@gmail.com>
2026-06-12 21:09:37 +0200

    Random Forest Regression

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # 1. Load the dataset and assign column headers
 # Import libraries
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+plt.close('all')
 
 # Load dataset
 url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/auto.csv"
@@ -68,7 +74,6 @@ Requirements:
         ▪ Mode replacement
 
 """
-import numpy as np
 df = df.replace('?',np.nan)
 print(df.head(10))
 
@@ -112,7 +117,6 @@ group_names = ['Low', 'Medium', 'High']
 df['horsepower-binned'] = pd.cut(df['horsepower'], bins, labels=group_names, include_lowest=True)
 print(df['horsepower-binned'].value_counts())
 
-import matplotlib.pyplot as plt
 # Normalizing length
 df['length'] = df['length'] / df['length'].max()
 
@@ -123,8 +127,7 @@ o Boxplot (price vs body-style)
 o Correlation heatmap
 
 """
-import seaborn as sns
-
+#Histograms
 plt.figure(figsize=(14, 5))
 
 # price
@@ -145,6 +148,7 @@ plt.ylabel('Count of Cars')
 plt.tight_layout()
 plt.clf()
 
+# Boxplot
 plt.figure(figsize=(7, 7))
 sns.boxplot(x='body-style', y='price', data=df)
 plt.clf()
@@ -155,3 +159,65 @@ numeric_df = df.corr(numeric_only=True)
 plt.figure(figsize=(12, 10))
 sns.heatmap(numeric_df, cmap="coolwarm", annot=True)
 plt.clf()
+
+"""
+TASK 3: Model Development (Regression) (LO1, LO2, LO3)
+Objective:
+Predict car price (continuous variable)
+
+Part A: Linear Regression
+1. Select features (e.g. horsepower, engine-size)
+2. Split data:
+o 80% training / 20% testing
+3. Train model:
+from sklearn.linear_model import LinearRegression
+4. Evaluate:
+• R² Score
+• MAE
+• MSE
+
+"""
+# Data split
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor
+
+plt.figure(figsize=(6, 4))
+sns.scatterplot(x='engine-size', y='price', data=df)
+plt.title('Exploratory Check: Horsepower vs Price')
+plt.clf()
+
+X, y = df[['horsepower']], df["price"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+r2 = model.score(X_test, y_test)
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+
+print("--- Part A: Linear Regression Results (80/20 Split) ---")
+print(f"R² Score: {r2:.4f}")
+print(f"MAE:      ${mae:.2f}")
+print(f"MSE:      {mse:.2f}")
+
+regressor = RandomForestRegressor(
+    n_estimators=100,
+    random_state=42,
+    oob_score=True
+)
+
+regressor.fit(X_train, y_train)
+print("Out-of-Bag Score:", regressor.oob_score_)
+
+y2_pred = regressor.predict(X_test)
+mae_2 = mean_absolute_error(y_test, y2_pred)
+mse_2 = mean_squared_error(y_test, y2_pred)
+r2_1 = r2_score(y_test, y2_pred)
+print(f"R-squared: {r2_1:.4f}")
+print(f"MAE:      ${mae_2:.2f}")
+print(f"MSE: {mse_2:.2f}")
